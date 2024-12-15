0d5ed34f573b2d3d11e3349392689284c64cef6c
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2024-12-15 12:25:30 +0530

    getBean by type(using className) , inner bean

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,12 +10,17 @@ public class App
         //using applicationcontext to create a obj of another class
         //applicationcontext contains a container where all objects are creted
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj= (Alien) context.getBean("alien");  //(Alien) typecasting
+//        Alien obj= (Alien) context.getBean("alien");  //(Alien) typecasting
+        Alien obj= context.getBean("alien", Alien.class);
 //        obj.setAge(13);
         System.out.println(obj.getAge());
         obj.code();
 //        Alien obj2= (Alien) context.getBean("alien");
 //        System.out.println(obj2.age);
 //        obj2.code();
+
+//        Desktop obj1=(Desktop) context.getBean("com");
+//        Desktop obj1=context.getBean("com",Desktop.class); //using without typecast
+        Desktop obj1=context.getBean(Desktop.class); //using without bean id
     }
 }
--- src/main/java/org/example/Desktop.java
+++ src/main/java/org/example/Desktop.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Desktop implements Computer{
+    Desktop(){
+        System.out.println("desktop created");
+    }
     @Override
     public void compile(){
         System.out.println("compiling desktop");
